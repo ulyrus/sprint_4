8f7a2035179a7863cd90ba9527b0bff14ecb09b0
Ulyana <alexandrov.nickolay2012@yandex.ru>
2024-07-13 14:58:31 +0300

    separated and moved tests

--- src/test/java/page_objects/MainPage.java
+++ src/test/java/page_objects/MainPage.java
@@ -58,22 +58,6 @@ public class MainPage {
         checkShownTextContainsTargetText(textFinder, expectedText);
     }
 
-    public void checkAccordeonsTextAppears() {
-        String[] expectedTexts = {
-                HOW_MUCH,
-                WANT_MULTIPLE,
-                HOW_CALCULATES_TIME,
-                CAN_ORDER_FOR_TODAY,
-                CAN_PROLONG_OR_BACK_EARLIER,
-                DELIVER_CHARGER,
-                CAN_CANCEL_ORDER,
-                MKAD
-        };
-        for (int index = 0; index < expectedTexts.length; index++) {
-            checkTextShowsOnAccordeonClick(index, expectedTexts[index]);
-        }
-    }
-
     public void onHeaderOrderButtonClick() {
         driver.findElement(orderButtonInHeader).click();
     }
@@ -84,12 +68,12 @@ public class MainPage {
         driver.findElement(orderButton).click();
     }
 
-    private static final String HOW_MUCH = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
-    private static final String WANT_MULTIPLE = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
-    private static final String HOW_CALCULATES_TIME = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
-    private static final String CAN_ORDER_FOR_TODAY = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
-    private static final String CAN_PROLONG_OR_BACK_EARLIER = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
-    private static final String DELIVER_CHARGER = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
-    private static final String CAN_CANCEL_ORDER = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
-    private static final String MKAD = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+    public static final String HOW_MUCH = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+    public static final String WANT_MULTIPLE = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+    public static final String HOW_CALCULATES_TIME = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+    public static final String CAN_ORDER_FOR_TODAY = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+    public static final String CAN_PROLONG_OR_BACK_EARLIER = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+    public static final String DELIVER_CHARGER = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+    public static final String CAN_CANCEL_ORDER = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+    public static final String MKAD = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
 }
